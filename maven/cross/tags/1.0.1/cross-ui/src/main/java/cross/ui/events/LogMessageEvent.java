340d59f86cb6d3ee90ed66749cb18e6a6d559490
Nils Hoffmann <nilshoffmann@users.sf.net>
2010-10-25 08:08:38 +0000

    [maven-release-plugin]  copy for tag 1.0.1

---
+++ maven/cross/tags/1.0.1/cross-ui/src/main/java/cross/ui/events/LogMessageEvent.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package cross.ui.events;
+
+import cross.event.AEvent;
+import cross.event.IEventSource;
+
+/**
+ * @author Nils.Hoffmann@CeBiTec.Uni-Bielefeld.DE
+ * 
+ * 
+ */
+public class LogMessageEvent extends AEvent<String> {
+
+	/**
+	 * @param v
+	 * @param ies
+	 */
+	public LogMessageEvent(final String v, final IEventSource<String> ies) {
+		super(v, ies);
+	}
+
+	@Override
+	public String getEventName() {
+		return "EVENT_MESSAGE_LOG";
+	}
+
+}
