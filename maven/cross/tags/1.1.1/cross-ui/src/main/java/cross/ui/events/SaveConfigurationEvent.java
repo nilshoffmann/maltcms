a2243bd38e32ce6ca509403dd34c474cba075cfa
Nils Hoffmann <nilshoffmann@users.sf.net>
2010-11-02 14:11:28 +0000

    [maven-release-plugin]  copy for tag 1.1.1

---
+++ maven/cross/tags/1.1.1/cross-ui/src/main/java/cross/ui/events/SaveConfigurationEvent.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package cross.ui.events;
+
+import org.apache.commons.configuration.Configuration;
+
+import cross.event.IEventSource;
+
+/**
+ * @author Nils.Hoffmann@CeBiTec.Uni-Bielefeld.DE
+ * 
+ * 
+ */
+public class SaveConfigurationEvent extends ConfigurationEvent {
+
+	/**
+	 * @param v
+	 * @param ies
+	 */
+	public SaveConfigurationEvent(final Configuration v,
+	        final IEventSource<Configuration> ies) {
+		super(v, ies);
+	}
+
+	@Override
+	public String getEventName() {
+		return "EVENT_CONFIGURATION_SAVE";
+	}
+
+}
