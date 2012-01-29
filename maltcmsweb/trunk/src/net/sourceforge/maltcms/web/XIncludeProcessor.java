172cd6b22708c224eb4e8c6d9a67736162282f7e
Nils Hoffmann <nilshoffmann@users.sf.net>
2012-01-29 15:11:44 +0000

    Update site contents and layout

--- maltcmsweb/trunk/src/net/sourceforge/maltcms/web/XIncludeProcessor.java
+++ maltcmsweb/trunk/src/net/sourceforge/maltcms/web/XIncludeProcessor.java
@@ -1,4 +1,5 @@
 package net.sourceforge.maltcms.web;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -23,73 +24,74 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-
 public class XIncludeProcessor {
-	
-	private final String in;
-	private final String out;
-	
-	public XIncludeProcessor(String in, String out) {
-		this.in = in;
-		this.out = out;
-	}
-	
-	public void process() {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		dbf.setNamespaceAware(true);
-		dbf.setXIncludeAware(true);
-		try {
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(new File(in));
-			
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			NodeList nodes = (NodeList) xpath.evaluate(
-			        "//@lastchanged", doc, XPathConstants.NODESET);
-			System.out.println("Found "+nodes.getLength()+" matching nodes for query!");
-			// Rename these nodes
-			for (int idx = 0; idx < nodes.getLength(); idx++) {
-				String id = nodes.item(idx).getTextContent();
-				
-				System.out.println("Replacing "+id);
-				id = id.replaceAll("\\$", "");
-				String[] split = id.split(" ");
-				System.out.println("with "+split[1]+" "+split[2]+" "+split[3]+" by "+split[11]);
-				nodes.item(idx).setTextContent(split[1]+" "+split[2]+" "+split[3]+" (GMT) by "+split[11]);
-			}
-			
-			OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(this.out)));
-			TransformerFactory tfactory = TransformerFactory.newInstance();
-        		Transformer serializer;
-		        try {
-            			serializer = tfactory.newTransformer();
-            			//Setup indenting to "pretty print"
-            			serializer.setOutputProperty(OutputKeys.INDENT, "yes");
-            			serializer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-            			
-            			serializer.transform(new DOMSource(doc), new StreamResult(out));
-        		} catch (TransformerException e) {
-            			// this is fatal, just dump the stack and throw a runtime exception
-            			e.printStackTrace();
-            
-        		}
-		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		} catch (SAXException sae) {
-			sae.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		} catch (XPathExpressionException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-        }
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Processing file: "+args[0]);
-		System.out.println("Writing result file: "+args[1]);
-		XIncludeProcessor xip = new XIncludeProcessor(args[0],args[1]);
-		xip.process();
-		System.out.println("Done!");
-	}
-}
 
+    private final String in;
+    private final String out;
+
+    public XIncludeProcessor(String in, String out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    public void process() {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        dbf.setXIncludeAware(true);
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new File(in));
+
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            NodeList nodes = (NodeList) xpath.evaluate(
+                    "//@lastchanged", doc, XPathConstants.NODESET);
+            System.out.println("Found " + nodes.getLength() + " matching nodes for query!");
+            // Rename these nodes
+            for (int idx = 0; idx < nodes.getLength(); idx++) {
+                String id = nodes.item(idx).getTextContent();
+                try {
+                    System.out.println("Replacing " + id);
+                    id = id.replaceAll("\\$", "");
+                    String[] split = id.split(" ");
+                    System.out.println("with " + split[1] + " " + split[2] + " " + split[3] + " by " + split[11]);
+                    nodes.item(idx).setTextContent(split[1] + " " + split[2] + " " + split[3] + " (GMT) by " + split[11]);
+                }catch(ArrayIndexOutOfBoundsException ae) {
+                    
+                }
+            }
+
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(this.out)));
+            TransformerFactory tfactory = TransformerFactory.newInstance();
+            Transformer serializer;
+            try {
+                serializer = tfactory.newTransformer();
+                //Setup indenting to "pretty print"
+                serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+                serializer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+                serializer.transform(new DOMSource(doc), new StreamResult(out));
+            } catch (TransformerException e) {
+                // this is fatal, just dump the stack and throw a runtime exception
+                e.printStackTrace();
+
+            }
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (SAXException sae) {
+            sae.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (XPathExpressionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Processing file: " + args[0]);
+        System.out.println("Writing result file: " + args[1]);
+        XIncludeProcessor xip = new XIncludeProcessor(args[0], args[1]);
+        xip.process();
+        System.out.println("Done!");
+    }
+}
