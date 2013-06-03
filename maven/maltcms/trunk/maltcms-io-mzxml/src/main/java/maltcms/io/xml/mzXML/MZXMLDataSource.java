0d931f967acecd2f314fb75376316e020076dd47
Nils Hoffmann <nilshoffmann@users.sf.net>
2013-06-03 09:08:49 +0200

    Fixed a regression due to recently removed MZXMLSaxDataSource.

--- maven/maltcms/trunk/maltcms-io-mzxml/src/main/java/maltcms/io/xml/mzXML/MZXMLDataSource.java
+++ maven/maltcms/trunk/maltcms-io-mzxml/src/main/java/maltcms/io/xml/mzXML/MZXMLDataSource.java
@@ -59,190 +59,194 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service = IDataSource.class)
 public class MZXMLDataSource implements IDataSource {
 
-    private final List<IDataSource> ds = new ArrayList<IDataSource>();
-    private final WeakHashMap<IFileFragment, IDataSource> fragmentToValidReaderMap = new WeakHashMap<IFileFragment, IDataSource>();
+	private final List<IDataSource> ds = new ArrayList<IDataSource>();
+	private final WeakHashMap<IFileFragment, IDataSource> fragmentToValidReaderMap = new WeakHashMap<IFileFragment, IDataSource>();
 
-    public MZXMLDataSource() {
-        final List<String> dss = Arrays.asList(
-                "maltcms.io.xml.mzXML.MZXMLStaxDataSource");
-        for (final String s : dss) {
-            this.ds.add(Factory.getInstance().getObjectFactory().instantiate(s,
-                    cross.io.IDataSource.class));
-        }
-    }
+	public MZXMLDataSource() {
+		final List<String> dss = Arrays.asList(
+				"maltcms.io.xml.mzXML.MZXMLStaxDataSource");
+		for (final String s : dss) {
+			this.ds.add(Factory.getInstance().getObjectFactory().instantiate(s,
+					cross.io.IDataSource.class));
+		}
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * cross.io.IDataSource#canRead(cross.datastructures.fragments.IFileFragment
-     * )
-     */
-    @Override
-    public int canRead(final IFileFragment ff) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * cross.io.IDataSource#canRead(cross.datastructures.fragments.IFileFragment
+	 * )
+	 */
+	@Override
+	public int canRead(final IFileFragment ff) {
 
-        // try {
-        // XMLReader reader = XMLReaderFactory.createXMLReader();
-        // reader.setContentHandler(new DefaultHandler() {
-        // public void startElement(String uri, String localName,
-        // String qName, Attributes attributes)
-        // throws SAXException {
-        // // System.out.println(qName);
-        // }
-        // });
-        // reader.parse(new InputSource(new FileInputStream(ff
-        // .getAbsolutePath())));
-        //
-        // } catch (SAXException e) {
-        // e.printStackTrace();
-        // } catch (FileNotFoundException e) {
-        // e.printStackTrace();
-        // } catch (IOException e) {
-        // e.printStackTrace();
-        // }
+		// try {
+		// XMLReader reader = XMLReaderFactory.createXMLReader();
+		// reader.setContentHandler(new DefaultHandler() {
+		// public void startElement(String uri, String localName,
+		// String qName, Attributes attributes)
+		// throws SAXException {
+		// // System.out.println(qName);
+		// }
+		// });
+		// reader.parse(new InputSource(new FileInputStream(ff
+		// .getAbsolutePath())));
+		//
+		// } catch (SAXException e) {
+		// e.printStackTrace();
+		// } catch (FileNotFoundException e) {
+		// e.printStackTrace();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
 
-        try {
-            final IDataSource ids = getValidReader(ff);
-            return 1;
-        } catch (final NotImplementedException nie) {
-        }
-        return 0;
-    }
+		try {
+			final IDataSource ids = getValidReader(ff);
+			return 1;
+		} catch (final NotImplementedException nie) {
+		}
+		return 0;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @seeorg.apache.commons.configuration.event.ConfigurationListener#
-     * configurationChanged
-     * (org.apache.commons.configuration.event.ConfigurationEvent)
-     */
-    @Override
-    public void configurationChanged(final ConfigurationEvent arg0) {
-        configure(Factory.getInstance().getConfiguration());
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.apache.commons.configuration.event.ConfigurationListener#
+	 * configurationChanged
+	 * (org.apache.commons.configuration.event.ConfigurationEvent)
+	 */
+	@Override
+	public void configurationChanged(final ConfigurationEvent arg0) {
+		configure(Factory.getInstance().getConfiguration());
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * cross.io.IDataSource#configure(org.apache.commons.configuration.Configuration
-     * )
-     */
-    @Override
-    public void configure(final Configuration configuration) {
-        final List<String> dss = StringTools.toStringList(configuration.getList(getClass().
-                getName() + ".dataSources", Arrays.asList(new Object[]{
-                "maltcms.io.xml.mzXML.MZXMLStaxDataSource"})));
-        for (final String s : dss) {
-            this.ds.add(Factory.getInstance().getObjectFactory().instantiate(s,
-                    cross.io.IDataSource.class));
-        }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * cross.io.IDataSource#configure(org.apache.commons.configuration.Configuration
+	 * )
+	 */
+	@Override
+	public void configure(final Configuration configuration) {
+		final List<String> dss = StringTools.toStringList(configuration.getList(getClass().
+				getName() + ".dataSources", Arrays.asList(new Object[]{
+			"maltcms.io.xml.mzXML.MZXMLStaxDataSource"})));
+		for (final String s : dss) {
+			if (s.equals("maltcms.io.xml.mzXML.MZXMLSaxDataSource")) {
+				log.warn("MZXMLSaxDataSource is no longer included in Maltcms as of version 1.3. It has been superseded by MZXMLStaxDataSource. Please remove MZXMLSaxDataSource from your configuration!");
+			} else {
+				this.ds.add(Factory.getInstance().getObjectFactory().instantiate(s,
+						cross.io.IDataSource.class));
+			}
+		}
 
-    }
+	}
 
-    private IDataSource getValidReader(final IFileFragment ff) {
-        if (this.fragmentToValidReaderMap.containsKey(ff)) {
-            return this.fragmentToValidReaderMap.get(ff);
-        }
-        for (final IDataSource ids : this.ds) {
-            try {
-                log.info("Checking DataSource {}", ids.getClass().getName());
-                if (ids.canRead(ff) > 0) {
-                    IDataSource dataSource = Factory.getInstance().getObjectFactory().instantiate(ids.getClass().getName(),
-                            cross.io.IDataSource.class);
-                    this.fragmentToValidReaderMap.put(ff, dataSource);
-                    return dataSource;
-                }
-            } catch (final RuntimeException e) {
-            }
-        }
-        throw new NotImplementedException("No provider available for "
-                + StringTools.getFileExtension(ff.getName()));
-    }
+	private IDataSource getValidReader(final IFileFragment ff) {
+		if (this.fragmentToValidReaderMap.containsKey(ff)) {
+			return this.fragmentToValidReaderMap.get(ff);
+		}
+		for (final IDataSource ids : this.ds) {
+			try {
+				log.info("Checking DataSource {}", ids.getClass().getName());
+				if (ids.canRead(ff) > 0) {
+					IDataSource dataSource = Factory.getInstance().getObjectFactory().instantiate(ids.getClass().getName(),
+							cross.io.IDataSource.class);
+					this.fragmentToValidReaderMap.put(ff, dataSource);
+					return dataSource;
+				}
+			} catch (final RuntimeException e) {
+			}
+		}
+		throw new NotImplementedException("No provider available for "
+				+ StringTools.getFileExtension(ff.getName()));
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * cross.io.IDataSource#readAll(cross.datastructures.fragments.IFileFragment
-     * )
-     */
-    @Override
-    public ArrayList<Array> readAll(final IFileFragment f) throws IOException,
-            ResourceNotAvailableException {
-        return getValidReader(f).readAll(f);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * cross.io.IDataSource#readAll(cross.datastructures.fragments.IFileFragment
+	 * )
+	 */
+	@Override
+	public ArrayList<Array> readAll(final IFileFragment f) throws IOException,
+			ResourceNotAvailableException {
+		return getValidReader(f).readAll(f);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @seecross.io.IDataSource#readIndexed(cross.datastructures.fragments.
-     * IVariableFragment)
-     */
-    @Override
-    public ArrayList<Array> readIndexed(final IVariableFragment f)
-            throws IOException, ResourceNotAvailableException {
-        return getValidReader(f.getParent()).readIndexed(f);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecross.io.IDataSource#readIndexed(cross.datastructures.fragments.
+	 * IVariableFragment)
+	 */
+	@Override
+	public ArrayList<Array> readIndexed(final IVariableFragment f)
+			throws IOException, ResourceNotAvailableException {
+		return getValidReader(f.getParent()).readIndexed(f);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @seecross.io.IDataSource#readSingle(cross.datastructures.fragments.
-     * IVariableFragment)
-     */
-    @Override
-    public Array readSingle(final IVariableFragment f) throws IOException,
-            ResourceNotAvailableException {
-        return getValidReader(f.getParent()).readSingle(f);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecross.io.IDataSource#readSingle(cross.datastructures.fragments.
+	 * IVariableFragment)
+	 */
+	@Override
+	public Array readSingle(final IVariableFragment f) throws IOException,
+			ResourceNotAvailableException {
+		return getValidReader(f.getParent()).readSingle(f);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @seecross.io.IDataSource#readStructure(cross.datastructures.fragments.
-     * IFileFragment)
-     */
-    @Override
-    public ArrayList<IVariableFragment> readStructure(final IFileFragment f)
-            throws IOException {
-        return getValidReader(f).readStructure(f);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecross.io.IDataSource#readStructure(cross.datastructures.fragments.
+	 * IFileFragment)
+	 */
+	@Override
+	public ArrayList<IVariableFragment> readStructure(final IFileFragment f)
+			throws IOException {
+		return getValidReader(f).readStructure(f);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @seecross.io.IDataSource#readStructure(cross.datastructures.fragments.
-     * IVariableFragment)
-     */
-    @Override
-    public IVariableFragment readStructure(final IVariableFragment f)
-            throws IOException, ResourceNotAvailableException {
-        return getValidReader(f.getParent()).readStructure(f);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecross.io.IDataSource#readStructure(cross.datastructures.fragments.
+	 * IVariableFragment)
+	 */
+	@Override
+	public IVariableFragment readStructure(final IVariableFragment f)
+			throws IOException, ResourceNotAvailableException {
+		return getValidReader(f.getParent()).readStructure(f);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see cross.io.IDataSource#supportedFormats()
-     */
-    @Override
-    public List<String> supportedFormats() {
-        final Set<String> al = new HashSet<String>();
-        for (final IDataSource ids : this.ds) {
-            al.addAll(ids.supportedFormats());
-        }
-        return new ArrayList<String>(al);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see cross.io.IDataSource#supportedFormats()
+	 */
+	@Override
+	public List<String> supportedFormats() {
+		final Set<String> al = new HashSet<String>();
+		for (final IDataSource ids : this.ds) {
+			al.addAll(ids.supportedFormats());
+		}
+		return new ArrayList<String>(al);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * cross.io.IDataSource#write(cross.datastructures.fragments.IFileFragment)
-     */
-    @Override
-    public boolean write(final IFileFragment f) {
-        return getValidReader(f).write(f);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * cross.io.IDataSource#write(cross.datastructures.fragments.IFileFragment)
+	 */
+	@Override
+	public boolean write(final IFileFragment f) {
+		return getValidReader(f).write(f);
+	}
 }
