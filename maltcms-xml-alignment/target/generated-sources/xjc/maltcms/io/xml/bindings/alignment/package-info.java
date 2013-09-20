e8863a0c55933f79a9a404d1de3572a3b9e23733
Nils Hoffmann <nilshoffmann@users.sf.net>
2013-09-20 11:55:48 +0200

    Performance fixes.

--- maltcms-xml-alignment/target/generated-sources/xjc/maltcms/io/xml/bindings/alignment/package-info.java
+++ maltcms-xml-alignment/target/generated-sources/xjc/maltcms/io/xml/bindings/alignment/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.5-2 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2013.09.03 um 03:57:56 PM CEST 
+// Generiert: 2013.09.18 um 06:01:26 PM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://maltcms.sourceforge.net/maltcmsAlignment", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
--- maltcms-xml-annotation/target/generated-sources/xjc/maltcms/io/xml/bindings/annotation/AnnotationType.java
+++ maltcms-xml-annotation/target/generated-sources/xjc/maltcms/io/xml/bindings/annotation/AnnotationType.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.5-2 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2013.09.03 um 03:57:58 PM CEST 
+// Generiert: 2013.09.18 um 05:51:36 PM CEST 
 //
 
 
